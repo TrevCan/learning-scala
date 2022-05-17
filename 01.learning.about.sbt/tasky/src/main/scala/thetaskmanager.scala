8f30612698caa7491a66e9782d08861c565d6ce2
a <a@example.org>
2022-05-17 14:31:52 -0500

    add task Manager methods.

--- 01.learning.about.sbt/tasky/src/main/scala/thetaskmanager.scala
+++ 01.learning.about.sbt/tasky/src/main/scala/thetaskmanager.scala
@@ -2,20 +2,28 @@ import java.time.LocalDate
 
 object TaskManager {
 
-  def getAllTasks(task_list: Seq[Task]): Seq[Task] = task_list.filter( t => true )
-
-  def getTasksBeforeToday(task_list: Seq[Task]): Seq[Task] = task_list.filter( t => LocalDate.now().isAfter( t.dueOn ) )
-
-  def getTasksAfterToday(task_list: Seq[Task]): Seq[Task] = task_list.filter( t => LocalDate.now().isBefore( t.dueOn ) )
-
-  def isAfter(task: Task, task_v: Task ): Boolean = task.dueOn.isAfter( task_v.dueOn )
-
-  def toString( tasks: Seq[Task] ): String = tasks.mkString ( sep: "iifaofao" )
+//  def getAllTasks(task_list: Seq[Task]): Seq[Task] = task_list.filter( t => true )
+//
+//  def getTasksBeforeToday(task_list: Seq[Task]): Seq[Task] = task_list.filter( t => LocalDate.now().isAfter( t.dueOn ) )
+//
+//  def getTasksAfterToday(task_list: Seq[Task]): Seq[Task] = task_list.filter( t => LocalDate.now().isBefore( t.dueOn ) )
+//
+//  def isAfter(task: Task, task_v: Task ): Boolean = task.dueOn.isAfter( task_v.dueOn )
+//
+//  def toString( tasks: Seq[Task] ): String = tasks.mkString ( sep: "iifaofao" )
   // def toString( tasks: Seq[Task] ): String = tasks.foreach( sT => sT.title ).toString 
 
   // def toString( tasks: Seq[Task] ): String = "aifjiafajofoaf"
 
   // ( for ( single_task <- tasks ) single_task.title
+  //
+
+  // this function just returns nothing or null
+  // def allTasksDueToday( taks: List[Task]): List[Task] = Nil
+  //
+
+  // this one actually filters the task list 
+  def allTasksDueToday( tasks: List[Task]): List[Task] = tasks.filter( p => LocalDate.now().isEqual( p.dueOn ) )
 
 
 
