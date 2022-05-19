e039cda8196580b2d25aaa781f1aaaeef67404bb
@trevcan Hector Canizales <heror 0484 [{at} protonmail ]dot[ com>
2022-05-19 16:37:01 -0500

    add stable version of balance( List[Char] )

--- exercise.0/recfun/src/main/scala/recfun/RecFun.scala
+++ exercise.0/recfun/src/main/scala/recfun/RecFun.scala
@@ -1,4 +1,5 @@
 package recfun
+import scala.util.control.Breaks._
 
 object RecFun extends RecFunInterface:
 
@@ -26,82 +27,21 @@ object RecFun extends RecFunInterface:
    */
   def balance(chars: List[Char]): Boolean = {
 
-    val onlyParths: String = chars.mkString.filter( x => { x == '(' || x == ')' } )
-    println("parths is ")
-    println(onlyParths)
+    val charsOnlyParths = chars.filter( char => char == '(' || char == ')' ).toList
 
-    if chars.isEmpty || chars.tail.isEmpty || onlyParths.length % 2 != 0 then false 
-    else {
-      if onlyParths.length == 2 then 
-        onlyParths.equals("()")
+    var index = 0;
+    var countOpenParentheses = 0;
 
-//      else {
-//        if onlyParths.startsWith("(") then
-//          val assumption = true
-//          val newTail = onlyParths.tail
-//          var trueCount: Int = 0
-//          while (! newTail.tail.isEmpty)
-//            if ( balance(onlyParths.tail) ) then trueCount = trueCount + 1
-//            newTail = newTail.tail
-//          //if (trueCount
-//
-//        else
-//          false
-//      }
-      else{
-        val halfParCount: Int = onlyParths.filter( x=> x == '(' ).length
-
-        printf("halfParCount is %s\n", halfParCount)
-
-        if halfParCount.equals(onlyParths.length / 2 ) then {
-          //onlyParths
-          if onlyParths.startsWith("(") then
-            println(s"the tail is ${onlyParths.tail}" );
-            balanceMore(onlyParths.tail.toList, true);
-          else
-            false
-
-        }
-        else
-          false
-      }
+    breakable{
+    for ( c <- charsOnlyParths )
+      if c == '(' then countOpenParentheses = countOpenParentheses + 1
+      else if countOpenParentheses == 0 then 
+        countOpenParentheses = countOpenParentheses - 1
+        break()
+      else countOpenParentheses = countOpenParentheses - 1
+      //println(c)
     }
-
-    def balanceMore( s: List[Char], ignoreFirstClosed: Boolean ): Boolean = {
-      if ( s.head == ')') then 
-        var count = 1
-        var sliced = s.mkString
-        sliced = sliced.slice(count, count+1)
-        while ( ! sliced.isEmpty ) {
-          count = count + 1
-          sliced = sliced.slice(count, count+1)
-          println(balance( sliced.toList ))
-        } 
-        true
-    else false
-
-    }
-
-    false
-  }
-
-    def checkInner( s: List[Char] ) : Boolean = {
-      true
-      // in here we know that we have an equal amount
-      // of open ( and closed ) parentheses.
-      // and we know that our string starts with (.
-      // we should now check wether we have other 
-      // unproperly closed or opened parentheses.
-      // that is, checking for position.
-      // e.g. ())(
-
-//      if ( s.length == 2 )
-//        return s.head.equals('(') && s.tail.head.equals(')')
-//      else
-//        //s.mkString.apply
-//
-
-    //}
+    return countOpenParentheses.equals(0)
 
   }
 
